c819c93d6f955b3f1caf5671fb88ad906a119ed8
Mackenzie Craig <hello@maccraig.net>
2018-07-16 14:46:39 -0700

    Get env variables from .env file in root of project.

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose').set('debug', true);
+var dotenv = require('dotenv').config()
 
 var app = express();
 
-// TODO: move stuff to .env
-var db_url = 'mongodb://localhost:27017/lfp';
+var db_url = process.env.DB_URL;
 var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
@@ -23,6 +23,7 @@ app.use(morgan('dev'));
 var db = mongoose.connect(db_url, {
   useNewUrlParser: true
 });
+
 var db_test = mongoose.connection;
 db_test.on('error', console.error.bind(console, 'connection error:'));
 db_test.once('open', function() {
